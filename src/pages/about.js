47330e9cf112eb6da4b1c21414e523cd22c06182
luucamay <lupemaydana@gmail.com>
2019-11-06 20:08:28 -0400

    Created main and about pages

---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <h1>About</h1>
+    <p>I am an indigenous aymara software engineer that permanently researchs that place where art meets technology</p>
+  </div>
+)
