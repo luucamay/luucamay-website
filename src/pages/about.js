c7b82f3d76282f3a38a1bcde113aba632bf19ac3
luucamay <lupemaydana@gmail.com>
2019-11-06 20:31:58 -0400

    Added header component to my site

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,10 @@
 import React from "react"
+import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `teal` }}>
-    <h1>About</h1>
+    <Header headerText="About" />
+    <Header headerText="Listening to my heart" />
     <p>I am an indigenous aymara software engineer that permanently researchs that place where art meets technology</p>
   </div>
 )
