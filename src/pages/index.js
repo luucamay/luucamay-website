3e41e2235f18360763cbb4a90cc68cdd8e6b5270
luucamay <lupemaydana@gmail.com>
2019-11-06 20:35:02 -0400

    Link between pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `purple` }}>
-    <h1>luucamay</h1>
+  <Link to="/contact/">Contact</Link>
+  <Header headerText="luu ca may" />
     <p>#imilldev</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
