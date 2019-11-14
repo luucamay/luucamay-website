11ac48b738c5745aafeaaf95d33de58c7ae7091e
luucamay <lupemaydana@gmail.com>
2019-11-13 21:34:13 -0400

    Added Typography.js plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,15 @@ import { Link } from "gatsby"
 import Header from "../components/header"
 
 export default () => (
-  <div style={{ color: `purple` }}>
+  <div>
     <Link to="/about/">About</Link>
     <Link to="/contact/">Contact</Link>
     <Header headerText="LUU CA MAY" />
     <p>#imilldev</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
   </div>
 )
